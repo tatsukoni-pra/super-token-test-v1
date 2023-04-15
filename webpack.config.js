fc212bf56e37b345da7944f6a70d009c106f4406
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-04-16 00:50:15 +0900

    fix: 調整

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.SUBGRAPH_RELEASE_TAG': JSON.stringify(process.env.SUBGRAPH_RELEASE_TAG),
     }),
   ],
 };
